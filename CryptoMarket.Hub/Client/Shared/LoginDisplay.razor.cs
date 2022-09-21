88a75bf56444e3ea002fea117c1f57d3d79a034d
Daniel <Daniel@DANIEL>
2022-09-21 22:58:30 +0200

    Adding and configuring Okta authentication

---
+++ CryptoMarket.Hub/Client/Shared/LoginDisplay.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CryptoMarket.Hub.Client.Shared
+{
+    public partial class LoginDisplay
+    {
+        [Inject] public NavigationManager Navigation { get; set; }
+        [Parameter] public string ReturnUrl { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ReturnUrl = Navigation.ToBaseRelativePath(Navigation.Uri);
+        }
+    }
+}
