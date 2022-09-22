5e83dfe08790a20912f67df9591c9e0d64c682b5
Daniel2001k <68070950+Daniel2001k@users.noreply.github.com>
2022-09-22 21:56:23 +0200

    Adding MudBlazor nuget, authentication changes, loading page changes (#3)
    Co-authored-by: Daniel <Daniel@DANIEL>

--- CryptoMarket.Hub/Client/Program.cs
+++ CryptoMarket.Hub/Client/Program.cs
@@ -2,6 +2,7 @@ using CryptoMarket.Hub.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -13,13 +14,15 @@ builder.Services.AddHttpClient("CryptoMarket.ServerAPI", client => client.BaseAd
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("CryptoMarket.ServerAPI"));
 
-builder.Services.AddOidcAuthentication(options =>
+builder.Services.AddOidcAuthentication<RemoteAuthenticationState, CustomUserAccount>(options =>
 {
     builder.Configuration.Bind("Okta", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
     options.ProviderOptions.DefaultScopes.Add("profile");
     options.ProviderOptions.DefaultScopes.Add("address");
-});
-builder.Services.AddApiAuthorization();
+}).AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, CustomUserAccount, CustomAccountFactory>();
+
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
